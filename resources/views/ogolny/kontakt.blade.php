0215faabe6811ae005fd8cb0b25d62f12a188657
Tomasz Nowacki <tomasz.nowacki@poznan.merito.pl>
2023-09-24 14:07:08 +0200

    tworzenie szablonów dla blade

---
+++ resources/views/ogolny/kontakt.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+@section('tytul')
+    WSB - Kontakt
+@endsection
+
+@section('podtytul')
+    Kontakt
+@endsection
+
+@section('tresc')
+    <div>Treść dla kontaktu </div>
+@endsection
---
+++ resources/views/ogolny/onas.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+@section('tytul')
+    WSB - O nas
+@endsection
+
+@section('podtytul')
+    O nas
+@endsection
+
+@section('tresc')
+    <div>Treść dla informacji o nas </div>
+@endsection
