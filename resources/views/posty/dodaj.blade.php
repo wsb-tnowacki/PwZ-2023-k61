ee6d2eabe315ded3c27d32df6406070096b190d5
Tomasz Nowacki <tomasz.nowacki@poznan.merito.pl>
2023-10-15 12:07:48 +0200

    grupowanie dla kontrolera i dodanie BS

---
+++ resources/views/posty/dodaj.blade.php
@@ -0,0 +1,14 @@
+@extends('layout')
+@section('tytul')
+    WSB - Dodanie posta
+@endsection
+
+@section('podtytul')
+    Formularz doddający post
+@endsection
+
+@section('tresc')
+<form action="" method="post">
+  
+</form>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,12 @@ use App\Http\Controllers\KontrolerStart;
 */
 
 
-Route::get('/', [KontrolerStart::class,'start'])->name('start');
+/* Route::get('/', [KontrolerStart::class,'start'])->name('start');
 Route::get('/kontakt', [KontrolerStart::class,'kontakt'])->name('kontakt');
-Route::get('/onas', [KontrolerStart::class,'onas'])->name('onas');
+Route::get('/onas', [KontrolerStart::class,'onas'])->name('onas'); */
+
+Route::controller(KontrolerStart::class)->group(function(){
+    Route::get('/', 'start')->name('start');
+    Route::get('/kontakt', 'kontakt')->name('kontakt');
+    Route::get('/onas', 'onas')->name('onas');
+});
